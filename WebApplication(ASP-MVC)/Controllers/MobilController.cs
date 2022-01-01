48bd176bbb131c96af565bee1468e319bd1b0cd2
dell <dell@DESKTOP-AJLDJKS>
2022-01-01 20:29:43 +0700

    Add project files.

---
+++ WebApplication(ASP-MVC)/Controllers/MobilController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_ASP_MVC_.Models;
+
+namespace WebApplication_ASP_MVC_.Controllers
+{
+    public class MobilController : Controller
+    {
+        public IActionResult Index()
+        {
+            /*var mobils = new List<Mobil>();
+
+            mobils.Add(new Mobil
+            {
+                IDRegistrasi = 1,
+                Tipe = "Sedan",
+                Merk = "Toyota",
+                Varian = "Apple",
+            });
+
+            mobils.Add(new Mobil
+            {
+                IDRegistrasi = 2,
+                Tipe = "Suv",
+                Merk = "Toyota",
+                Varian = "Rav4",
+            });
+*/
+            var banyakMobil = new Mobil[]
+            {
+                new Mobil{  IDRegistrasi = 0001, Tipe= "Sedan", Merk = "Toyota", Varian = "FT86"  },
+                new Mobil{  IDRegistrasi = 0002, Tipe = "SUV", Merk = "Toyota", Varian = "RAV4"  },
+                new Mobil{  IDRegistrasi = 0003, Tipe = "Sedan", Merk = "Honda", Varian = "Accord"  },
+                new Mobil{  IDRegistrasi = 0004, Tipe = "SUV", Merk = "Honda", Varian = "CRV"  },
+                new Mobil{  IDRegistrasi = 0005, Tipe = "Sedan", Merk = "Honda", Varian = "City"  },
+
+            };
+
+            var cariMobil = banyakMobil.Where(x => x.Tipe == "Sedan");
+
+
+            string nama = "Edward";
+            ViewBag.namaSaya = nama;
+            ViewBag.mobils = cariMobil;
+            
+
+            //ViewData["nama"] = "Edwardddd";
+
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication(ASP-MVC)/Models/Mobil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_ASP_MVC_.Models
+{
+    public class Mobil
+    {
+        public int IDRegistrasi { get; set; }
+        public string Tipe { get; set; }
+        public string Merk { get; set; }
+        public string Varian { get; set; }
+        
+    }
+}
